762d2c49fcba02fe5983d05ae800ea5ab365c74e
onurravli <onurravli@hotmail.com>
2023-02-26 23:44:52 +0300

    sdkfhsdkj

--- api/index.py
+++ api/index.py
@@ -2,10 +2,16 @@ from flask import Flask
 from random import randrange
 import json
 
-app = Flask(__name__)
 facts = json.load(open("facts.json", "r"))
+
+app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return facts[randrange(len(facts))]
+
+
+@app.route('/about')
+def about():
+    return 'About'
